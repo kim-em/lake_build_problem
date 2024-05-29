6ec496693d251b61c70b472784a13963e8cbe7d8
Kim Morrison <kim@tqft.net>
2024-05-29 19:05:50 +1000

    initial state

---
+++ BuildProblems.lean
@@ -0,0 +1,1 @@
+import BuildProblems.A
---
+++ lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package «build_problems» where
+  -- add package configuration options here
+
+@[default_target]
+lean_lib «BuildProblems» where
+  -- add library configuration options here
